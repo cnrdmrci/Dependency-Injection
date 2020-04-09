9982c847d84b3e8388d52c1ede5102915328f8f0
cnrdmrci <canerdemirci.mail@gmail.com>
2020-04-09 18:02:28 +0300

    IoC container added

---
+++ IoC_Container.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+
+namespace DependencyInjection.IoC_Container
+{
+    public class IoC_Container
+    {
+        public static void Run()
+        {
+            IContainer container = getAutofacConfigFromPrograModule();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var icecekMakinesi = scope.Resolve<IcecekMakinesi>();
+                //IcecekMakinesi icecekMakinesi = new IcecekMakinesi(container.Resolve<IIcecek>());
+                icecekMakinesi.Olustur();
+            }
+
+        }
+
+        public static IContainer getAutofacConfigFromPrograModule()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<ProgramModule>();
+            IContainer container = builder.Build();
+            return container;
+        }
+
+        public static IContainer getAutofacConfigContainer()
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+            builder.RegisterType<IcecekMakinesi>().AsSelf();
+            builder.RegisterType<Kahve>().As<IIcecek>();
+            IContainer container = builder.Build();
+
+            return container;
+        }
+    }
+
+    public interface IIcecek
+    {
+        void Hazirla();
+    }
+    public class Kahve : IIcecek
+    {
+        public void Hazirla()
+        {
+            Console.WriteLine("Kahve hazırlanıyor.");
+        }
+    }
+    public class IcecekMakinesi
+    {
+        IIcecek _icecek;
+        public IcecekMakinesi(IIcecek icecek)
+        {
+            _icecek = icecek;
+        }
+        public void Olustur()
+        {
+            _icecek.Hazirla();
+        }
+    }
+}
