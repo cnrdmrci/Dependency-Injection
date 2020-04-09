b5effaa27f4c27e0b50c63bb2d7a17a44f135c85
cnrdmrci <canerdemirci.mail@gmail.com>
2020-04-09 18:02:46 +0300

    ioc container configuration module added

---
+++ ProgramModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Autofac;
+using Module = Autofac.Module;
+
+namespace DependencyInjection.IoC_Container
+{
+    public class ProgramModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<IcecekMakinesi>().AsSelf();
+            builder.RegisterType<Kahve>().As<IIcecek>();
+        }
+    }
+}
